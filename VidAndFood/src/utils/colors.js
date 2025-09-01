c054d19f9641b1a58c047c2612d3d45e13228dcf
mauricio tempone <143232326+mauritempo@users.noreply.github.com>
2025-09-01 10:10:56 -0300

    nav-bar Working for implementation.

---
+++ VidAndFood/src/utils/colors.js
@@ -0,0 +1,12 @@
+const COLORS = {
+  green: '#284907',
+  red: '#641220',
+  white: '#FFFFFF',
+  lightBackground: '#FDF7F2', 
+  darkRed: '#4A0D15', 
+  darkGreen: '#1A3305', 
+  grey: '#1A1A1A',
+
+};
+
+export default COLORS;
